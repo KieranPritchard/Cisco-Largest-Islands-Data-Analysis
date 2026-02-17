c1d528001eb18a07564f04068b2ef45a0c4b09c0
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-17 19:22:34 +0000

    added bar chart to project

--- largest_islands.py
+++ largest_islands.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Reads in the data from the largest islands file
 islands = pd.read_csv('./largest-islands.csv')
@@ -11,3 +13,11 @@ island_list = islands["island"]
 
 # Extracts the area and stores them
 area = islands["area"]
+
+# Plots the x and y values
+x = np.array(island_list)
+y = np.array(area)
+
+# Outputs a bar chart
+plt.barh(x, y)
+plt.show()
