454b376fe41cd00960217f4c614394bff53a30eb
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-17 19:18:35 +0000

    added pandas data parsing

--- largest_islands.py
+++ largest_islands.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+# Reads in the data from the largest islands file
+islands = pd.read_csv('./largest-islands.csv')
+
+# Sorts the files based on area and ascending
+islands = islands.sort_values(by="area", ascending=True)
+
+# Extracts the islands and stores them
+island_list = islands["island"]
+
+# Extracts the area and stores them
+area = islands["area"]
